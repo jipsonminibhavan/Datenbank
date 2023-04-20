527461e693b2459c75528b221a503b6c80ebf030
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-04-20 17:15:36 +0200

    feat init routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
 const express = require("express");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); //Multer-Upload-Objekt; neue Multer Instanz wird erzeugt.
+/*
+const upload = multer({
+  dest: 'uploads/',
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ['application/pdf'];
+    if (!allowedTypes.includes(file.mimetype)) {
+      const error = new Error('Nur PDF-Dateien sind erlaubt');
+      error.code = 'LIMIT_FILE_TYPES';
+      return cb(error, false);
+    }
+    cb(null, true);
+  }
+});
+ -Nur Dateitypen als PDFs hochladen,
+ => multer-Upload-Objekt konfigurieren und eine fileFilter-Funktion schreiben die nur PDF-Dateien zulässt
+ -fileFilter-Funktion verwendet, dass nur Dateien mit dem MIME-Typ application/pdf hochgeladen werden
+*/
+
 app.use(bodyParser.json());
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.json({ Name: "jipson" });
 });
+
+/*
+-Multer-DiskStorage-Instanz, um den Speicherort 
+ und den Dateinamen zu definieren an dem die hochgeladene Datei gespeichert wird
+
+-Verzeichnis "uploads/date/id" mit dem ursprünglichen Dateinamen als Suffix.
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = `./uploads/${req.params.date}`;
+    fs.mkdirSync(dir, { recursive: true });<--
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${req.params.id}${path.extname(file.originalname)}` 
+    (in diesem Fall für pdf:  const fileName = `${req.params.id}.pdf`);
+    cb(null, fileName);
+
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.pdf');
+  }
+});
+
+const upload = multer({ storage: storage });
+*/
 
 app.get("/documents", async (req, res) => {
   const { Document } = require("./models/DocumentModel");
@@ -45,6 +98,64 @@ app.post("/documents", async (req, res) => {
     console.error(error);
     res.json({ msg: error });
   }
+});
+
+app.post("/upload", upload.single("document"), (req, res, next) => {
+  console.log(req.file);
+  next();
+  // req.file is the `doc` file
+  // req.body will hold the text fields, if there were any
+  //upload.single technisch betrachtet die Middleware, vorherige Schritte ist Konfig.
+});
+
+/*--> POST-Route, um die PDF-Datei hochzuladen:
+app.post('/upload', upload.single('pdf'), (req, res) => {
+  res.send('PDF-Datei wurde erfolgreich hochgeladen!');
+});<----
+
+---> GET-Route, um die PDF-Datei basierend auf dem Datum und der ID zurückzugeben:
+app.get('/download/:date/:id', (req, res) => {
+  const date = req.params.date;
+  const id = req.params.id;
+  const filepath = `uploads/${date}/${id}.pdf`;
+
+  res.setHeader('Content-Type', 'application/pdf');
+  res.sendFile(filepath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('PDF-Datei nicht gefunden');
+    }
+  });
+}); <-----
+
+ */
+//- PDF-Dateien im Ordner uploads gespeichert/ gespeichert /Datum und ID benannt
+
+app.get("/upload/:id", (req, res) => {
+  const id = req.params.id;
+  const filepath = `${process.cwd()}/uploads/${id}`; // absoluter Pfad ()
+  res.setHeader("Content-Type", "application/pdf");
+  res.sendFile(filepath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send("PDF-Datei nicht gefunden");
+    }
+    return filepath;
+  });
+});
+
+app.delete("/upload/:id", (req, res) => {
+  const id = req.params.id;
+  const filepath = `${process.cwd()}/uploads/${id}`;
+
+  fs.unlink(filepath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send("Datei nicht gefunden");
+    } else {
+      res.status(204).send("Datei wurde erfolgreich gelöscht");
+    }
+  });
 });
 
 const start = async () => {
